4661d3fb3874558ea7c2f8740ffaa797aab3782d
lstech2021 <haddy2014@163.com>
2024-07-06 17:58:44 +0800

    inital code from weui

---
+++ CHANGELOG.md
@@ -0,0 +1,124 @@
+# 更新日志
+
+## 1.3.1
+
+- fix: 移除 babel 依赖
+
+## 1.3.0
+
+- feat: skyline 兼容
+- feat: 升级 `weui-wxss` 至 2.6.6
+
+## 1.2.10
+
+- fix: half-screen-dialog 恢复滚动
+
+## 1.2.9
+
+- fix: dialog 修复 z-index 层级
+
+## 1.2.8
+
+- fix: searchbar 修复聚焦状态
+
+## 1.2.7
+
+- fix: dialog 修复自动聚焦 outline #216
+- fix: half-screen-dialog 修复自动聚焦 outline #216
+- fix: actionsheet 修复自动聚焦 outline #216
+- fix: gallery 修复自动聚焦 outline #216
+
+## 1.2.6
+
+- feat: dialog 使用 virtualHost
+- feat: 升级 `weui-wxss` 至 2.5.15
+
+## 1.2.5
+
+- feat: checkbox 添加 disabled 属性
+- fix: tabbar 修复 aria-label 告警 #206
+- fix: slideview 修复 disabled 属性 #203
+- fix: actionsheet 移除多余 wrap
+- fix: dialog 移除多余 wrap
+
+## 1.2.4
+
+- feat: 升级 `weui-wxss` 至 2.5.14
+- fix: 修复 fade 动画
+
+## 1.2.3
+
+- fix: 修复 half-screen-dialog button 样式向前兼容
+
+## 1.2.2
+
+- feat: tabbar 添加 reactive 选项
+- fix: 修复 actionsheet 底部 safearea 高度不正确
+- fix: 修复 gallery 底部 safearea 高度不正确
+## 1.2.1
+
+- fix: 修复 checkbox 无法操作
+
+## 1.2.0
+
+- feat: 升级 `weui-wxss` 至 2.5.0
+- feat: 无障碍适配
+- fix: 修复 slideview 隐藏按钮后无动画
+- fix: 修复 checkbox-group wx:if 判断不正确
+## 1.1.1
+
+- fix: 修复找不到 wxs
+- fix: 修复找不到 weui-wxss
+
+## 1.1.0
+
+- feat: 升级 `weui-wxss` 至 2.4.4
+- fix: 防止 `mp-half-screen-dialog` 滚动穿透
+- chore: 构建系统迁移至 mpflow
+
+## 1.0.8
+
+- fix: 修复 #153
+- fix: 修复 `mp-searchbar` 点击取消时错误 focus
+- fix: 修复 `mp-half-screen-dialog` 按钮在部分机型按钮排列出错
+
+## 1.0.7
+
+- fix: 修复 `mp-icon` 在 type 改变时报错
+
+## 1.0.6
+
+- feat: `mp-grids` 支持动态修改 grids
+- feat: `mp-searchbar` 点击取消按钮时触发 `cancel` 事件
+- fix: 修复 `mp-half-screen-dialog` 中的 desc 属性判断
+- fix: 修复 `mp-searchbar` 节流搜索没有获取最新数据
+- fix: 修复 `form-validator` 中 bytelength 报错
+
+## 1.0.5
+
+- updata: 升级 weui-wxss
+- update：使用 `hover-class` 代替 `:active`
+
+## 1.0.4
+
+- fix: 修复 rangelength 校验
+- feat: 支持自适应 tabbar
+
+## 1.0.3
+
+- fix: `required` 判断 (#93)
+- fix: dialog 未居中 (#94)
+
+## 1.0.2
+
+- fix: Toptips 不展示的bug
+- fix: Form 校验失效的bug
+
+## 1.0.1
+
+- 补充 package.json repository
+
+## 1.0.0
+
+- 更新`weui-wxss`到`2.3.0`版本，支持 DarkMode
+- Demo 支持 DarkMode
---
+++ src/components/slideview/slideview.ts
@@ -0,0 +1,229 @@
+Component({
+    /**
+     * 组件的属性列表
+     */
+    options: {
+        multipleSlots: true
+    },
+    properties: {
+        extClass: {
+            type: String,
+            value: ''
+        },
+        buttons: {
+            type: Array,
+            value: [], // type, data, text, src, extClass
+            observer() {
+                this.addClassNameForButton()
+            }
+        },
+        disable: {
+            type: Boolean,
+            value: false
+        },
+        icon: {
+            // 是否是icon
+            type: Boolean,
+            value: false
+        },
+        show: {
+            type: Boolean,
+            value: false
+        },
+        duration: {
+            type: Number,
+            value: 350 // 动画市场，单位ms
+        },
+        throttle: {
+            type: Number,
+            value: 40
+        },
+        rebounce: {
+            type: Number,
+            value: 0 // 回弹距离
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        size: null
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    ready() {
+        // @ts-ignore
+        this.updateRight()
+        this.addClassNameForButton()
+        // 共享元素 this.st
+        this.st = {
+            movex: wx.worklet.shared(0),
+            max: wx.worklet.shared(0),
+            startX: wx.worklet.shared(0),
+            startY: wx.worklet.shared(0),
+            out: wx.worklet.shared(false), 
+            isMoving: wx.worklet.shared(false), 
+            disable: wx.worklet.shared(false), 
+            firstAngle: wx.worklet.shared(0),
+            size: {
+                buttons: wx.worklet.shared([]),
+            },
+            transformx: wx.worklet.shared(0),
+            transformTotal: wx.worklet.shared([])
+        }
+        this.initAnimate()
+    },
+    methods: {
+        updateRight() {
+            // 获取右侧滑动显示区域的宽度
+            const data: any = this.data
+            const query = this.createSelectorQuery()
+            query
+                .select('.left')
+                .boundingClientRect((res) => {
+                    const btnQuery = this.createSelectorQuery()
+                    btnQuery
+                        .selectAll('.btn')
+                        .boundingClientRect((rects) => {
+                            this.setData({
+                                size: {
+                                    buttons: rects,
+                                    button: res,
+                                    show: data.show,
+                                    disable: data.disable,
+                                    throttle: data.throttle,
+                                    rebounce: data.rebounce
+                                }
+                            })
+                        })
+                        .exec()
+                })
+                .exec()
+        },
+        addClassNameForButton() {
+            // @ts-ignore
+            const { buttons, icon } = this.data
+            buttons.forEach((btn) => {
+                if (icon) {
+                    btn.className = ''
+                } else if (btn.type === 'warn') {
+                    btn.className = 'weui-slideview__btn-group_warn'
+                } else {
+                    btn.className = 'weui-slideview__btn-group_default'
+                }
+            })
+            this.setData({
+                buttons
+            })
+        },
+        buttonTapByWxs(data) {
+            this.triggerEvent('buttontap', data, {})
+        },
+        hide() {
+            this.triggerEvent('hide', {}, {})
+        },
+        show() {
+            this.triggerEvent('show', {}, {})
+        },
+        transitionEnd() {},
+        
+        // 动画，从 wxs 迁移过来
+        // 用 this.st 来存滑动状态
+        initAnimate() {
+            // 左侧动画
+            this.applyAnimatedStyle('.weui-slideview__left', () => {
+                'worklet';
+                if (this.st.movex.value >= 0) {
+                    // 往回滑
+                    this.st.transformx.value = this.st.movex.value - this.st.max.value
+                    if (!this.st.isMoving.value) this.st.transformx.value = 0
+                } else {
+                    this.st.transformx.value = this.st.movex.value
+                    if (!this.st.isMoving.value) this.st.transformx.value = -this.st.max.value
+                }
+                return {
+                    'transform': 'translateX(' + this.st.transformx.value + 'px)',
+                    'transition': ''
+                }   
+            })
+
+            // 按钮动画
+            let transformTotal = 0
+            this.createSelectorQuery().selectAll('.btn').boundingClientRect((rects) => {
+                // 记录单个按钮最大值，总的最大值为所有按钮宽度的和
+                var total = 0
+                var buttons = []
+                for (let i = rects.length - 1; i >= 0; i--) {
+                    total += rects[i].width
+                    buttons[i] = total
+                    this.st[`transformTotal-${i}`] = wx.worklet.shared(0)
+                }
+                this.st.transformTotal.value = transformTotal
+                this.st.size.buttons.value = buttons
+                this.st.max.value = total
+                
+                for (let index = rects.length - 1; index >= 0; index--) {
+                    this.applyAnimatedStyle(`.btn-${index}`, () => {
+                        'worklet';
+                        var transform = rects[index].width / this.st.max.value * this.st.movex.value
+                        // 0 固定就是 0
+                        var transformt = this.st[`transformTotal-${index}`].value
+
+                        var transformx
+                        
+                        if (this.st.movex.value >= 0) {
+                            // 往回滑
+                            transformx = -(this.st.size.buttons.value[index] - Math.min(this.st.size.buttons.value[index], transform + transformt))
+                            if (!this.st.isMoving.value) transformx = 0
+                        } else {
+                            transformx = Math.max(-this.st.size.buttons.value[index], transform + transformt)
+                            if (!this.st.isMoving.value) transformx = -this.st.size.buttons.value[index]
+                        }
+
+                        // 计算下一个的值
+                        if (index > 0) this.st[`transformTotal-${index - 1}`].value = this.st[`transformTotal-${index}`].value + transform
+
+                        return {
+                            'transform': `translateX(${transformx}px)`,
+                            'transition': ''
+                        }   
+
+                    }) 
+                    
+                }
+            }).exec()
+        },
+        touchstart(event) {
+            'worklet';
+            
+            if (this.st.disable.value) return // disable的逻辑
+            this.st.startX.value = event.touches[0].pageX
+            this.st.startY.value = event.touches[0].pageY
+            this.st.firstAngle.value = 0
+        },
+        touchmove(event) {
+            'worklet';
+        
+            var pagex = event.touches[0].pageX - this.st.startX.value
+            var pagey = event.touches[0].pageY - this.st.startY.value
+            // 左侧45度角为界限，大于45度则允许水平滑动
+            if (this.st.firstAngle.value === 0) {
+                this.st.firstAngle.value = Math.abs(pagex) - Math.abs(pagey)
+            }
+            if (this.st.firstAngle.value < 0) {
+                return
+            }
+
+            this.st.isMoving.value = true
+            this.st.movex.value = pagex > 0 ? Math.min(this.st.max.value, pagex) : Math.max(-this.st.max.value, pagex)
+            
+            return false // 禁止垂直方向的滑动
+        },
+        touchend(event) {
+            this.st.isMoving.value = false
+        }
+    }
+})
